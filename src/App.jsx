e47c268089118b8b0e4187c459752880f7d32d8f
Christopher Nunez <elgary1129@gmail.com>
2024-04-08 21:53:12 -0400

    Persistencia con local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
 import "./App.css";
-import confetti from "canvas-confetti"
+import confetti from "canvas-confetti";
 import { Square } from "./components/Square.jsx";
 import { TURNS } from "./constants.js";
 import { checkWinnerFrom } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStrorage = window.localStorage.getItem("board");
+    return boardFromStrorage
+      ? JSON.parse(boardFromStrorage)
+      : Array(9).fill(null);
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    // si no hay local storage inicia el x
+    return turnFromStorage ?? TURN.X;
+  });
 
   const [winner, setWinner] = useState(null);
-
- 
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
   };
 
   const updateBoard = (index) => {
@@ -31,10 +42,13 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    // guardar partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", turn);
     // revisa si hay gabnador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
-      confetti()
+      confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
       setWinner(false); // empate
@@ -42,8 +56,8 @@ function App() {
   };
 
   const checkEndGame = () => {
-    return newBoard.every((square) => square !== null)
-  }
+    return newBoard.every((square) => square !== null);
+  };
 
   //
 
@@ -67,7 +81,7 @@ function App() {
           <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
         </section>
 
-        <WinnerModal winner={winner} resetGame={resetGame}/>
+        <WinnerModal winner={winner} resetGame={resetGame} />
       </main>
     </>
   );
